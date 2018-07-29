3e40f7924a1ccf7d884cf00e3f6267617a22c899
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-28 19:43:54 -0500

    added test to see the error of reducer

--- tests/reducers/root-reducer-test.js
+++ tests/reducers/root-reducer-test.js
@@ -3,10 +3,18 @@ import rootReducer from '../../src/reducers'
 import { FETCH_LOADING } from '../../src/types'
 
 describe('suite testing root reducer', () => {
-  it('should return nextState with property page.fetch.loading:true', () => {
+  it('should return nextState with property page1.fetch.loading:true', () => {
     const action = { type: FETCH_LOADING }
     const state = {}
     const nextState = rootReducer(state, action)
     expect(nextState.page1.fetch.loading).toBeTruthy()
+  })
+
+  it('should return nextState with property page1.fetch.loading:true and page2.fetch.loading:false', () => {
+    const action = { type: FETCH_LOADING }
+    const state = {}
+    const nextState = rootReducer(state, action)
+    expect(nextState.page1.fetch.loading).toBeTruthy()
+    expect(nextState.page1.fetch.loading).toBeFalsy()
   })
 })
