5f93777a2d62024c69aa7729e01316a5e0865b07
DezkaReid <elmaildeldezkareid@gmail.com>
2018-07-28 22:04:34 -0500

    Added high order reducer

--- src/reducers/page2/index.js
+++ src/reducers/page2/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
 import fetchReducer from '../fetch'
+import HighOrderReducer from '../high-order-reducer'
 
+const meta = { initialState: {}, key: 'page2-fetch-reducer' }
+const newFetchReducer = HighOrderReducer(meta, fetchReducer)
 const page2Reducer = combineReducers({
-  fetch: fetchReducer
+  fetch: newFetchReducer
 })
 
 export default page2Reducer
